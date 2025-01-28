90b7440b0ab2aa16a673ff1779b53e116727e20a
Taher Master <master.taherm@gmail.com>
2025-01-28 01:31:54 -0500

    Initial commit 2 for deployment

---
+++ src/app/activity3/page.tsx
@@ -0,0 +1,7 @@
+export default function Activity2Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold">Welcome to Crickit Match</h1>
+    </div>
+  );
+}
